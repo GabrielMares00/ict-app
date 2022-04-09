3135af604aa03a022f3783257cb78ce71904200b
GabrielMares00 <gabriel.mares00@e-uvt.ro>
2022-04-09 21:35:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import user_interface.startup as UI
+
+if __name__ == "__main__":
+    UI.start_user_interface()
---
+++ user_interface/startup.py
@@ -0,0 +1,7 @@
+import tkinter
+
+
+def start_user_interface():
+    mainWindow = tkinter.Tk()
+
+    mainWindow.mainloop()
